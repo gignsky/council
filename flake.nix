178c76c330cbc0dd3c106888cde45e2377d25333
Maxwell Rupp <maxwell@giglab.dev>
2026-09-15 19:54:27 -0400

    added rf

--- flake.nix
+++ flake.nix
@@ -6,8 +6,14 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
 
@@ -22,7 +28,10 @@
 
           src = ./site;
 
-          nativeBuildInputs = [ pkgs.zola pkgs.html-tidy ];
+          nativeBuildInputs = [
+            pkgs.zola
+            pkgs.html-tidy
+          ];
 
           buildPhase = ''
             runHook preBuild
@@ -81,6 +90,7 @@
             python3
             git
             html-tidy
+            roll-flow
           ];
           shellHook = ''
             echo "  Council dev shell"
@@ -89,5 +99,6 @@
             echo "  serve output:   nix run"
           '';
         };
-      });
+      }
+    );
 }
