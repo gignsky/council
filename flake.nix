4e50f4161e01e91f9c2d46eaae754b7c259617d7
Maxwell Rupp <maxwell@giglab.dev>
2026-09-15 19:53:09 -0400

    changed to gigpkgs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Council — the landis.fish static site (Zola)";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:gignsky/gigpkgs/gigpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
